40465006247a29bca6a7df35caac1eb4a80fd9e9
Layauu <80603850+Layauu@users.noreply.github.com>
2022-11-21 22:18:29 +0100

    Ajout de MainForm

---
+++ Projet_Poo/MainForm.cpp
@@ -0,0 +1,13 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    //WinformCDemo is jour project name
+    ProjetPoo::MainForm form;
+    Application::Run(% form);
+}
