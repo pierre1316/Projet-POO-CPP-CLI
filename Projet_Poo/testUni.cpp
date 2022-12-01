33cc0de30c3e6b4e90d801cea1f0c667fe726804
Baptiste <baptistedelavenne80@gmail.com>
2022-12-01 11:29:16 +0100

    update

--- Projet_Poo/testUni.cpp
+++ Projet_Poo/testUni.cpp
@@ -5,24 +5,20 @@ using namespace std;
 using namespace System;
 using namespace System::Data;
 testUni::testUni() {
-	this->name = "";
+	this->first_name = "";
 	this->last_name = "";
 	this->sup = NULL;
-	this->hire_date = "";
 	this->adress = "";
 	this->CP = NULL;
 	this->city = "";
 }
 
-testUni::testUni(char t_name, char t_last_name, int t_sup, string t_hire_date, char t_adress, int t_CP, char t_city) {
-	this->name = t_name;
+testUni::testUni(char t_first_name, char t_last_name, int t_sup, string t_hire_date, char t_adress, int t_CP, char t_city) {
+	this->first_name = t_first_name;
 	this->last_name = t_last_name;
 	this->sup = t_sup;
-	this->hire_adress = t_hire_date;
 	this->adress = t_adress;
 	this->CP = t_CP;
 	this->city = t_city;
-
-
 }
 
--- Projet_Poo/testUni.h
+++ Projet_Poo/testUni.h
@@ -3,11 +3,15 @@
 #include <iostream>
 using namespace std;
 
+private: System::Data::DataSet^ oDs;
+private: NS_Comp_Svc::CLservPeople^ oPeople;
+private: NS_Comp_Svc::CLservStaff^ oStaff;
+private: NS_Comp_Svc::CLservAddress^ oAddress;
 
 namespace ProjetPoo {
 
 	private :
-		char name;
+		char first_name;
 		char last_name;
 		int sup;
 		string hire_date;
@@ -16,7 +20,28 @@ namespace ProjetPoo {
 		char city;
 	public:
 		testUni();
-		testUni(char t_name, char t_last_name, int t_sup,string t_hire_date char t_adress, int t_CP, char t_city);
-		~testuni()
+		testUni(char t_first_name, char t_last_name, int t_sup,string t_hire_date char t_adress, int t_CP, char t_city);
+		~testUni()
 
+		System::Data::DataRow^ row;
+		int idPeople;
+		row = this->oPeople->createPeople(t_first_name, t_last_name);
+		idPeople = System::Convert::ToInt32(row->ItemArray[0]);
+
+private:
+	System::Data::DataRow^ row;
+		// Créer un staff
+	int indexStaff;
+			if (this->combo_superior->SelectedIndex != -1) {
+				this->oDs = this->oStaff->selectAllStaff("staff");
+				indexStaff = this->combo_superior->SelectedIndex;
+				sup = System::Convert::ToInt32(this->oDs->Tables["staff"]->Rows[indexStaff]->ItemArray[0]);
+			}
+		System::DateTime^ date = t_hire_date;
+		System::String^ dateString = System::Convert::ToString(date->Month) + "/";
+		dateString += System::Convert::ToString(date->Day) + "/";
+		dateString += System::Convert::ToString(date->Year);
+		this->oStaff->createStaff(idPeople, dateString, sup, "");
+		int idAddress = this->oAddress->createAddress("", "", adress, CP, city);
+		this->oStaff->updateStaffAddress(idPeople, idAddress);
 }
